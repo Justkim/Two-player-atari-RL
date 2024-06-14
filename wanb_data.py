3cf9da297394cbdaf6a8fce48b872618632ed31d
kimiya <kimiya.saadat97@gmail.com>
2024-06-14 13:02:47 -0600

    add changes

--- wanb_data.py
+++ wanb_data.py
@@ -2,19 +2,26 @@ import pandas as pd
 import wandb
 import pandas
 import csv
+import sys
+import os
+from tqdm import tqdm
+from os import path
 api = wandb.Api(timeout=29)
 
 data_list = []
-
+directory_path = sys.argv[1]
+run_link = sys.argv[2]
+print(run_link)
 # run is specified by <entity>/<project>/<run_id>
-run = api.run("justkim42/machin_transfer/bcu51q0d")
-history = run.scan_history(keys=['episode', 'total_reward'], page_size=5000)
-# history = run.history(keys=['episode', 'total_reward'], pandas=True)
-# history.to_csv("y6kjzffd_sample.csv")
-with open('bcu51q0d.csv', 'w', newline='') as file:
+run = api.run(run_link)
+history = run.scan_history(keys=['episode', 'total_reward'], page_size=1000000)
+if not os.path.exists(directory_path):
+    os.makedirs(directory_path)
+with open(path.join('/home/kimiya/Projects/Two-player-atari-RL/',directory_path, run.name + ".csv"), 'w', newline='') as file:
+    print(run.name)
     writer = csv.writer(file)
-    writer.writerow(['episode', 'total_reward'])
+    writer.writerow([run.name])
+    counter = 0
     for entry in history:
-        print(entry)
-        data_list.append(entry)
-        writer.writerow([entry['episode'], entry['total_reward']])
+        writer.writerow([entry['total_reward']])
+        counter += 1
