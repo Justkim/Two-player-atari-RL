78849953c6642113fed395395c6a1a09585539e2
kimiya <kimiya.saadat97@gmail.com>
2024-06-21 13:44:24 -0600

    add new annotations

--- utils/annotations.py
+++ utils/annotations.py
@@ -1,85 +1,79 @@
 atari_annotations = {
     "space_invaders_v2":  dict(    
     player_x=[28, 29],
-    player_y=[104, 105],
-    score=[73, 74]
+    player_score=[104, 105],
+    player_lives=[73, 74]
     ),
     "pong_v3":  dict(    
     player_x=[45, 46],
     player_y=[50, 51],
-    score=[13, 14]
+    player_score=[13, 14]
     ),
     "boxing_v2":  dict(    
     player_x=[32, 33],
     player_y=[34, 35],
-    score=[18, 19]
+    player_score=[18, 19]
     ),
     "tennis_v3":  dict(    
     player_x=[26, 27],
     player_y=[24, 25],
-    score=[69, 70]
+    player_score=[69, 70]
     ),
     "surround_v2":  dict(    
     player_x=[111, 112],
     player_y=[109, 110],
-    score=[118, 119]
+    player_score=[118, 119]
     ),
     #Needs editing after
     "mario_bros_v3":  dict(    
-    player_x=[111, 112],
-    player_y=[109, 110],
-    score=[118, 119]
+    player_x=[44, 45],
+    player_y=[42, 43],
+    score_higher_bits=[9, 11],
+    score_lower_bits=[10, 12],
+    player_lives=[7, 8]
     ),
     "wizard_of_wor_v3":  dict(    
-    player_x=[111, 112],
-    player_y=[109, 110],
-    score=[118, 119]
+    player_x=[54, 55],
+    player_score=[5, 6]
     ),
     "video_checkers_v4":  dict(    
-    player_x=[111, 112],
-    player_y=[109, 110],
-    score=[118, 119]
+
     ),
     "double_dunk_v3":  dict(    
-    player_x=[111, 112],
-    player_y=[109, 110],
-    score=[118, 119]
+    outside_player_x=[39, 40],
+    inside_player_x=[37, 38],
+    outside_player_y=[35, 36],
+    inside_player_y=[33, 34]
+    player_score=[118, 119]
     ),
     "flag_capture_v2":  dict(    
-    player_x=[111, 112],
-    player_y=[109, 110],
-    score=[118, 119]
+    player_x=[0, 1],
+    player_y=[2, 3],
+    player_score=[106, 107]
     ),
     "othello_v3":  dict(    
-    player_x=[111, 112],
-    player_y=[109, 110],
-    score=[118, 119]
+    turn=[64] #01=1, FF=255 #add to code
     ),    
     "entombed_competitive_v3":  dict(    
-    player_x=[111, 112],
-    player_y=[109, 110],
-    score=[118, 119]
+    player_x=[50, 51],
+    player_y=[48, 49],
+    lives_bits=[71] #code,
+    player_potion=[98, 99]
+   
     ),
-    "entombed_cooperative_v3":  dict(    
-    player_x=[111, 112],
-    player_y=[109, 110],
-    score=[118, 119]
+    "entombed_cooperative_v3":  dict(     #The same as the above game
+    player_x=[50, 51],
+    player_y=[48, 49],
+    lives_bits=[71] #code,
+    player_potion=[98, 99]
+   
     ),
     "ice_hockey_v2":  dict(    
-    player_x=[111, 112],
-    player_y=[109, 110],
-    score=[118, 119]
-    ),
-    "double_dunk_v3":  dict(    
-    player_x=[111, 112],
-    player_y=[109, 110],
-    score=[118, 119]
+
     ),
     "space_war":  dict(    
-    player_x=[28, 29],
-    player_y=[104, 105],
-    score=[73, 74]
-    ),
+
+    )
 
     
     
