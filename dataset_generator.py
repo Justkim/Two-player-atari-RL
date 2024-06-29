69cade7f5f774c9c2519c5a3f9d9df2e1b488e3e
kimiya <kimiya.saadat97@gmail.com>
2024-06-28 18:55:16 -0600

    add updates

--- dataset_generator.py
+++ dataset_generator.py
@@ -6,6 +6,7 @@ import torch.nn as nn
 import torch.optim as optim
 import numpy as np
 import wandb
+import supersuit as ss
 import gymnasium as gym
 from datetime import datetime
 import argparse
@@ -14,75 +15,115 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import numpy as np
+from utils.agent_indication_atari_wrapper import AgentIndicatorAtariEnv
 from pettingzoo.atari import pong_v3, double_dunk_v3, flag_capture_v2, entombed_cooperative_v3, entombed_competitive_v3, tennis_v3, space_invaders_v2, mario_bros_v3, surround_v2, boxing_v2
 from tqdm import tqdm
+from atari_wrapper import make_atari_env
+
+
+def reset(env):
+    if args.task == "space_invaders":
+        observations, infos = env.reset(seed=99)
+        for i in range(130):
+            actions = {'first_0': 0, 'second_0': 0}
+            observations, rewards, terminations, truncations, infos = env.step(actions)
+    elif args.task == "pong":
+        observations, infos = env.reset(seed=99)
+        for i in range(60):
+            actions = {'first_0': 0, 'second_0': 0}
+            observations, rewards, terminations, truncations, infos = env.step(actions)
+        
+    else:
+        observations, infos = env.reset(seed=99)
+    return observations, infos
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     np.random.seed(99)
     random.seed(99)
     parser.add_argument("--num-player", type=int, default=1)
-    parser.add_argument("--task-name", type=str, default='')
+    parser.add_argument("--task", type=str, default='')
     args = parser.parse_args()
     if args.num_player == 1:
-        if args.task_name == "pong":
-            env1 = gym.make('Pong-ramNoFrameskip-v4')
-        elif args.task_name == "space_invaders":
-            env1 = gym.make('SpaceInvaders-ramNoFrameskip-v4')
-        elif args.task_name == "mario_bros":
-            env1 = gym.make('ALE/MarioBros-ram-v5')
-        elif args.task_name == "surround":
-            env1 = gym.make('ALE/Surround-ram-v5')
-        elif args.task_name == "tennis":
-            env1 = gym.make('Tennis-ramNoFrameskip-v4')
-        elif args.task_name == "double_dunk":
-            env1 = gym.make('DoubleDunk-ramNoFrameskip-v4')
-        elif args.task_name == "entombed":
-            env1 = gym.make('ALE/Entombed-ram-v5')
-        elif args.task_name == "boxing":
-            env1 = gym.make('Boxing-ramNoFrameskip-v4')
-        elif args.task_name == "flag_capture":
-            env1 = gym.make('ALE/FlagCapture-ram-v5')
-
-
-
-        else:
-            exit()
-    elif args.num_player == 2:
-        if args.task_name == "pong":
-            env2 = pong_v3.parallel_env(obs_type='ram')
-        elif args.task_name == "space_invaders":
-            env2 = space_invaders_v2.parallel_env(obs_type='ram')
-        elif args.task_name == "boxing":
-            env2 = boxing_v2.parallel_env(obs_type='ram')
-        elif args.task_name == "tennis":
-            env2 = tennis_v3.parallel_env(obs_type='ram')
-        elif args.task_name == "surround":
-            env2 = surround_v2.parallel_env(obs_type='ram')
-        elif args.task_name == "mario_bros":
-            env2 = mario_bros_v3.parallel_env(obs_type='ram')
-        elif args.task_name == "flag_capture":
-            env2 = flag_capture_v2.parallel_env(obs_type='ram', full_action_space=True)
-        elif args.task_name == "entombed_competitive":
-            env2 = entombed_competitive_v3.parallel_env(obs_type='ram')
-        elif args.task_name == "entombed_cooperative":
-            env2 = entombed_cooperative_v3.parallel_env(obs_type='ram')
-        elif args.task_name == "double_dunk":
-            env2 = double_dunk_v3.parallel_env(obs_type='ram')
+        if args.task == "pong":
+            env1 = 'Pong-ramNoFrameskip-v4'
+        elif args.task == "space_invaders":
+            env1 = 'SpaceInvaders-ramNoFrameskip-v4'
+        elif args.task == "mario_bros":
+            env1 = 'ALE/MarioBros-ram-v5'
+        elif args.task == "surround":
+            env1 = 'ALE/Surround-ram-v5'
+        elif args.task == "tennis":
+            env1 = 'Tennis-ramNoFrameskip-v4'
+        elif args.task == "double_dunk":
+            env1 = 'DoubleDunk-ramNoFrameskip-v4'
+        elif args.task == "entombed":
+            env1 ='ALE/Entombed-ram-v5'
+        elif args.task == "boxing":
+            env1 = 'Boxing-ramNoFrameskip-v4'
+        elif args.task == "flag_capture":
+            env1 = 'ALE/FlagCapture-ram-v5'
         
+
+
         else:
             exit()
+
+        env1, train_envs, test_envs = make_atari_env(
+        env1,
+        99,
+        1,
+        1,
+        scale=False,
+        frame_stack=False,
+        mode='ram'
+    
+    )
+
+
+    elif args.num_player == 2:
+        if args.task == "pong":
+            env2 = pong_v3.parallel_env(obs_type='ram')
+        elif args.task == "space_invaders":
+            env2 = space_invaders_v2.parallel_env(obs_type='ram')
+        elif args.task == "boxing":
+            env2 = boxing_v2.parallel_env(obs_type='ram')
+        elif args.task == "tennis":
+            env2 = tennis_v3.parallel_env(obs_type='ram')
+        elif args.task == "surround":
+            env2 = surround_v2.parallel_env(obs_type='ram')
+        elif args.task == "mario_bros":
+            env2 = mario_bros_v3.parallel_env(obs_type='ram')
+        elif args.task == "flag_capture":
+            env2 = flag_capture_v2.parallel_env(obs_type='ram', full_action_space=True)
+        elif args.task == "entombed_competitive":
+            env2 = entombed_competitive_v3.parallel_env(obs_type='ram')
+        elif args.task == "entombed_cooperative":
+            env2 = entombed_cooperative_v3.parallel_env(obs_type='ram')
+        elif args.task == "double_dunk":
+            env2 = double_dunk_v3.parallel_env(obs_type='ram')
+        else:
+            exit()
+        env2 = ss.frame_skip_v0(env2, 4)
+        # # repeat_action_probability is set to 0.25 to introduce non-determinism to the system
+        env2 = ss.sticky_actions_v0(env2, repeat_action_probability=0.25)
+        # env2 = AgentIndicatorAtariEnv(env2)
+        # env2 = ss.dtype_v0(env2, np.dtype("float64"))
+        # env2 = ss.normalize_obs_v0(env2)
+        # env2 = ss.clip_reward_v0(env2)
+
 
 
     args = parser.parse_args()
 
-    batch_size = 5000
+    batch_size = 50000
     batch = []
     if args.num_player == 1:
         env1.reset(seed=99)
     else:
-        env2.reset(seed=99)
-
+        reset(env2)
+    inner_step = 0
     for step in tqdm(range(batch_size)):
         if args.num_player == 1:
             action1 = env1.action_space.sample()
@@ -93,15 +134,17 @@ if __name__ == "__main__":
         elif args.num_player == 2:
             action2 =   {'first_0': env2.action_space('first_0').sample(), 'second_0': env2.action_space('second_0').sample()}
             observation2, rew2, term, trunc, _ = env2.step(action2)
-            observation2 = observation2["first_0"]
-            if term or trunc:
-                env2.reset(seed=99)
+            observation2 = observation2["second_0"]
+            inner_step += 1
+            if term['first_0'] or trunc['first_0'] or inner_step >= 200:
+                reset(env2)
+                inner_step = 0
             batch.append(observation2)
     batch_array = np.array(batch)
     if args.num_player == 1:
-        np.savez('ram_datasets/one_player_dataset_{}.npz'.format(args.task_name), dataset=batch_array)
+        np.savez('ram_datasets/one_player_dataset_{}.npz'.format(args.task), dataset=batch_array)
     elif args.num_player == 2:
-        np.savez('ram_datasets/two_player_dataset_{}.npz'.format(args.task_name), dataset=batch_array)
+        np.savez('ram_datasets/two_player_dataset_{}.npz'.format(args.task), dataset=batch_array)
     # data_loaded = np.load('one_player_dataset_{}.npz'.format(args.task_name))
     # large_board_dataset_loaded = data_loaded['big_dataset']
     # print(large_board_dataset_loaded.shape)
--- heatmap.py
+++ heatmap.py
@@ -4,6 +4,18 @@ import matplotlib
 import matplotlib.pyplot as plt
 import argparse
 
+
+def load(dataset_name):
+    data_loaded = np.load('ram_datasets/{}_player_dataset_{}.npz'.format(dataset_name,args.task_name))
+    dataset_loaded = data_loaded['dataset']
+    return dataset_loaded
+
+def calculate_temporal_variance(dataset_loaded):
+
+    variance = average_calc(dataset_loaded, dataset_loaded.shape[0], kernel_size=11)
+    variance = np.reshape(variance, (32, 4))
+    return variance
+
 def single_byte_average_calc(data, x, kernel_size):
     sum = 0
     # for y in range(kernel_size):
@@ -29,52 +41,35 @@ def average_calc(data, data_size, kernel_size=3):  # kernel_size is assumed to b
     return diff_sum / (data_size - kernel_size + 1)
 parser = argparse.ArgumentParser()
 parser.add_argument("--task-name", type=str, default='')
+parser.add_argument("--num-player", type=int, default=0)
 parser.add_argument("--clip", default=False, action='store_true')
+
 args = parser.parse_args()
 
-data_loaded = np.load('ram_datasets/{}_player_dataset_{}.npz'.format('one',args.task_name))
-one_player_dataset_loaded = data_loaded['dataset']
+if args.num_player < 2:
+    one_player_dataset_loaded = load('one')
+    one_variance = calculate_temporal_variance(one_player_dataset_loaded)
 
+if args.num_player  == 0 or args.num_player == 2:
+    two_player_dataset_loaded = load('two')
+    two_variance = calculate_temporal_variance(two_player_dataset_loaded)
 
-# for i in tqdm(range(100000)):
-#     print(one_player_dataset_loaded[i])
-
-errors = average_calc(one_player_dataset_loaded, one_player_dataset_loaded.shape[0], kernel_size=100)
-
-# data_loaded = np.load('two_player_dataset.npz')
-# two_player_dataset_loaded = data_loaded['big_dataset']
-errors = np.reshape(errors, (32, 4))
-# tan_errors = np.tan(errors)
-# log_error = np.emath.logn(100, errors)
-clip_errors = np.clip(errors, a_min = 0, a_max = 500)
-
-print(one_player_dataset_loaded[21])
-
-data_loaded = np.load('ram_datasets/{}_player_dataset_{}.npz'.format('two',args.task_name))
-two_player_dataset_loaded = data_loaded['dataset']
-print(two_player_dataset_loaded[21])
-print("sdfsdsdfsdfsef")
-
-# for i in tqdm(range(100000)):
-#     print(one_player_dataset_loaded[i])
-
-errors2 = average_calc(two_player_dataset_loaded, two_player_dataset_loaded.shape[0], kernel_size=100)
-
-# data_loaded = np.load('two_player_dataset.npz')
-# two_player_dataset_loaded = data_loaded['big_dataset']
-errors2 = np.reshape(errors2, (32, 4))
-# tan_errors = np.tan(errors)
-# log_error = np.emath.logn(100, errors)
-if args.clip:
-    ferrors = np.clip(np.abs(errors - errors2), a_min = 0, a_max = 500)
-else:
-    ferrors = np.abs(errors - errors2)
-    
-print(errors)
-print(errors2)
-print("--------")
-print(np.abs(errors - errors2))
-print(args.task_name + ": " + str(np.sum(np.abs(errors - errors2))/128))
-plt.imshow(ferrors, cmap='hot', interpolation='nearest')
-plt.savefig("heatmaps/" + args.task_name + str(args.clip) + "_heatmap_difference.png")
-# plt.show()
+if args.num_player == 0:
+    if args.clip:
+        ferrors = np.clip(np.abs(one_variance - two_variance), a_min = 0, a_max = 500)
+    else:
+        ferrors = np.abs(one_variance - two_variance)
+    plt.imshow(ferrors, cmap='hot')
+    print(args.num_player, args.task_name, np.sum(ferrors)/128)
+    plt.savefig("heatmaps/" + args.task_name + str(args.clip) + "_error_heatmap.png")
+if args.num_player == 1:
+    plt.imshow(one_variance, cmap='hot')
+    print(args.num_player, args.task_name, np.sum(one_variance)/128)
+    plt.savefig("heatmaps/" + args.task_name + "_ram_one_heatmap.png")
+if args.num_player == 2:
+    print("-------------------")
+    print(args.num_player, args.task_name, np.sum(two_variance)/128)
+    # two_variance = (two_variance-np.min(two_variance))/(np.max(two_variance)-np.min(two_variance)) 
+    # print(two_variance)
+    plt.imshow(two_variance, cmap='hot')
+    plt.savefig("heatmaps/" + args.task_name + "_ram_two_heatmap.png")
